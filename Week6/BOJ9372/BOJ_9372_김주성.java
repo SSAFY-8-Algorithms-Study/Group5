75792a312910e3faff0cc4e5830ff8f0d033a84f
김주성 <kjschocolate@gmail.com>
2022-09-02 00:38:03 +0900

    BOJ_9372_김주성

---
+++ Week6/BOJ9372/BOJ_9372_김주성.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < M; i++) {
+                br.readLine();
+            }
+            System.out.println(N - 1);
+        }
+    }
+}
