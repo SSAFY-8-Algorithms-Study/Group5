f503a1f0fb2cd395ea3b42d133ebab689404b575
Leeh9 <40704078+Leeh9@users.noreply.github.com>
2022-09-13 18:26:57 +0900

    BOJ_9655_이현구

---
+++ Week7/BOJ9655/BOJ_9655_이현구.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_9655 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N =sc.nextInt();
+		
+		if(N %2 ==1) {
+			System.out.println("SK");
+		}else {
+			System.out.println("CY");
+		}
+	}
+}
