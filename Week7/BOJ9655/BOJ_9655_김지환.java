62bbfb31a0cdfb97f9147b754291dca38c14a52e
wlghks960 <98100778+wlghks960@users.noreply.github.com>
2022-09-13 19:18:42 +0900

    Update BOJ_9655_김지환.java

--- Week7/BOJ9655/BOJ_9655_김지환.java
+++ Week7/BOJ9655/BOJ_9655_김지환.java
@@ -1,5 +1,13 @@
 package day0912;
 
+import java.util.Scanner;
+
 public class BOJ_9655_김지환 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int stone = sc.nextInt();
+		System.out.println(stone%2==1?"SK":"CY");
+	}
 
 }
